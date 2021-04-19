ccda243a8a7735b279b5b65397e51040dc1da789
yerim seo <yerim7386@naver.com>
2021-04-19 14:43:20 +0900

    branchB주석

--- GitStudy/src/exam/Question2.java
+++ GitStudy/src/exam/Question2.java
@@ -6,5 +6,8 @@ public class Question2 {
 		System.out.println(Math.random());
 		
 		System.out.println("Random!!");
+		
+		// branchA, B 모두에 커밋을 2개 추가해보기
+		// (단, Question2만 수정해야 함 ※ 이 주석은 branchB에서 작성되었음)
 	}
 }
