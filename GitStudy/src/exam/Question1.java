543412eb94af95b126dc56ea73597ebe71b3c56f
user <user@DESKTOP-916S7V7>
2021-04-19 11:34:41 +0900

    문제1 풀고 저장

---
+++ GitStudy/src/exam/Question1.java
@@ -0,0 +1,10 @@
+package exam;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		System.out.println(" /)/)");
+		System.out.println("( ..)");
+		System.out.println("( >$)");
+	}
+}
