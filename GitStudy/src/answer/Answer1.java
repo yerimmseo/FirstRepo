096cd5b6be792917dbda3409e6b93e26fb0d21e2
yerim seo <yerim7386@naver.com>
2021-04-20 10:15:07 +0900

    answer1

--- GitStudy/src/answer/Answer1.java
+++ GitStudy/src/answer/Answer1.java
@@ -3,7 +3,8 @@ package answer;
 public class Answer1 {
 	public static void main(String[] args) {
 		System.out.println("Á¤´ä1");
-		System.out.println("Answer2");
+		System.out.println("Answer1");
+		System.out.println("Answer1");
 	}
 
 }
