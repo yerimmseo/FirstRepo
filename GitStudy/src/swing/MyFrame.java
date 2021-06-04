6da745585f5221aa0227e91fa5ef9236e46b40c3
yerimmseo <80576569+yerimmseo@users.noreply.github.com>
2021-06-04 15:08:37 +0900

    Initial commit

---
+++ GitStudy/src/swing/MyFrame.java
@@ -0,0 +1,14 @@
+package swing;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	
+	public MyFrame() {
+		setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setBounds(500, 50, 600, 600);
+		setVisible(true);
+	}
+
+}
---
+++ GitStudy/src/swing/S05_GridLayout.java
@@ -0,0 +1,29 @@
+package swing;
+
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+
+public class S05_GridLayout extends MyFrame {
+	
+	public S05_GridLayout() {
+		ArrayList<JButton> btns = new ArrayList<>(10);
+		
+		for (int i = 0; i < 14; i++) {
+			btns.add(new JButton("Button" + i));
+		}
+		
+		setLayout(new GridLayout(2, 7, 15, 15));
+		
+		for (JButton btn : btns) {
+			add(btn);
+		}
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new S05_GridLayout();
+	}
+
+}
